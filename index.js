ba23edeb9b960657442808752bb4d51f5a06dfe8
HNeal1337 <haydenneal1337@gmail.com>
2022-02-09 20:43:15 -0500

    Update index.js
    made a change

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+}
+dogImages.style.transform.rotate('180deg')
+let dogName = document.querySelectorAll('#dog-name')
+dogName.style.textAlign = 'left'
+let footerText = document.querySelector('#footer')
+footerText.style.color = 'blue'
 
 // Select some elements...
